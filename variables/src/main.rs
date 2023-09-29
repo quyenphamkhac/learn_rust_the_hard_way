3b4953c650c722f0284c2b13a0481d1c8c62d433
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2023-09-29 23:25:09 +0700

    feat: rust variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    let y = "       ";
+    let y = y.len();
+    println!("The length is {y}")
 }
