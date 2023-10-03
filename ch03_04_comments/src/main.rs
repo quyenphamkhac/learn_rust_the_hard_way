d8c23aeb9d142923bd536092fd0ded12e9f1c531
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2023-10-03 15:12:26 +0700

    feat: rust comments

---
+++ ch03_04_comments/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // This is a single comment
+    println!("Hello, world!");
+
+    // Multiple
+    // comments
+    // here
+
+    // I'm feeling lucky today
+    let lucky_number = 7;
+    println!("Lucky number is: {lucky_number}"); // Me too
+}
